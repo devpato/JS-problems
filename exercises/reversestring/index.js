3535a393e113958e773bf316fda4c7576eadcf23
Patricio Vargas <pato@Patricios-MacBook-Pro.local>
2019-08-27 00:14:05 -0400

    string reverse v2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,12 +2,17 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 reverse = (str) => {
-    return str.split('').reverse().join('');
+    //return str.split('').reverse().join('');
+    let reversed = '';
+    for( let character of str) {
+        reversed = character + reversed;
+    }
+    return reversed;
 }
 
 module.exports = reverse;
