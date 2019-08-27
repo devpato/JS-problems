a662f7300744ea795e3f6673f075cc4d0c6b1617
Patricio Vargas <pato@Patricios-MacBook-Pro.local>
2019-08-27 00:21:05 -0400

    first problem done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,13 +7,17 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 reverse = (str) => {
+    //V1
     //return str.split('').reverse().join('');
+    
+    //V2
     // let reversed = '';
     // for( let character of str) {
     //     reversed = character + reversed;
     // }
     // return reversed;
 
+    //V3
     return str.split('').reduce((reversed, character) => character + reversed, '');
 }
 
