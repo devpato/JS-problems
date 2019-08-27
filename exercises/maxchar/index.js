6f1d39862234341dae52b6e60dbcade67d3e8b01
Patricio Vargas <pato@Patricios-MacBook-Pro.local>
2019-08-27 01:36:33 -0400

    max character used solution v2

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -10,21 +10,28 @@ maxChar = (str) => {
     let max = 0;
     let maxChar = '';
     //Solution v1
-    for(let char of str) {     
-        if(!obj[char]) {
-            obj[char] = 1
-        } else {
-            obj[char]++;
-        }
-    }
+    // for(let char of str) {     
+    //     if(!obj[char]) {
+    //         obj[char] = 1
+    //     } else {
+    //         obj[char]++;
+    //     }
+    // }
     
-    for ( let char in obj) {
-        if ( obj[char] > max) {
-            max = obj[char];
-            maxChar = char;
-        }
-    }
-    return maxChar;
+    // for ( let char in obj) {
+    //     if ( obj[char] > max) {
+    //         max = obj[char];
+    //         maxChar = char;
+    //     }
+    // }
+
+    //Solution V2
+    str.split('').map(c=>{
+        obj[c] ? obj[c]++ : obj[c] = 1 
+    })
+
+    const keys = Object.keys(obj);
+    return keys.reduce((acc, curr) => obj[curr] > obj[acc] ? curr : acc, keys[0] );
 }
 
 module.exports = maxChar;
