ddcac2055f18ccb3f697addfae3b7aa7c292964e
Patricio Vargas <pato@Patricios-MacBook-Pro.local>
2019-08-27 21:59:49 -0400

    anagrams v2 solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -10,20 +10,44 @@
 
 anagrams = (stringA, stringB) => {
   //V1
-  return (
-    stringA
-      .replace("/[^w]/g")
-      .toLowerCase()
-      .split("")
-      .sort()
-      .join() ===
-    stringB
-      .replace("/[^w]/g")
-      .toLowerCase()
-      .split("")
-      .sort()
-      .join()
-  );
+  //   return (
+  //     stringA
+  //       .replace("/[^w]/g")
+  //       .toLowerCase()
+  //       .split("")
+  //       .sort()
+  //       .join() ===
+  //     stringB
+  //       .replace("/[^w]/g")
+  //       .toLowerCase()
+  //       .split("")
+  //       .sort()
+  //       .join()
+  //   );
+
+  //V2
+  const aCharMap = buildingCharMap(stringA);
+  const bCharMap = buildingCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+buildingCharMap = str => {
+  const charMap = {};
+  for (let char of str.replace("/[^w]/g").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  return charMap;
 };
 
 module.exports = anagrams;
